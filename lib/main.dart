e8f5c6ff3c294bbc21daaea5507662e86ffdeca8
DuaSpoolios <duaspall372332@gmail.com>
2025-09-29 18:23:00 -0400

    basic code

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mood Toggle Challenge',
+      theme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Mood Toggle Challenge')),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('How are you feeling?', style: TextStyle(fontSize: 24)),
+            SizedBox(height: 30),
+            Placeholder(fallbackHeight: 150, fallbackWidth: 150),
+          ],
+        ),
+      ),
+    );
+  }
+}
